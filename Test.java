cbbdc78f2f991e3f9490397ddeb72dd46d92806a
kraju411 <k.raju411@gmail.com>
2022-08-09 21:24:28 +0530

    Update Test.java
    Update code

--- Test.java
+++ Test.java
@@ -1,6 +1,7 @@
 public class Test{
 	public static void main(String ar[]){
-		System.println("From Test class");
+		System.out.println("From Test class");
+		System.out.println("second line");
 	}
 	
 }
